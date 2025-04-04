0f2424f72c2ada7591eee47871db352c3ac2251d
Lucas Chambi <lucasbrunocc880@outlook.com>
2025-04-03 22:27:02 -0300

    Add files via upload

---
+++ TutoriaisJS/numbers.js
@@ -0,0 +1,26 @@
+let n1 = 200
+let n2 = 401
+
+console.log(n1)
+console.log(n1, n2)
+console.log(n1 + n2)
+
+let soma = n1 + n2
+console.log(`${n1} + ${n2} = ${soma}`)
+
+let subtracao = n1 - n2
+console.log(`${n1} - ${n2} = ${subtracao}`)
+
+let multiplicacao = n1 * n2
+console.log(`${n1} x ${n2} = ${multiplicacao}`)
+
+let divisao = n1 / n2
+console.log(`${n1} / ${n2} = ${divisao}`)
+
+let potencia = Math.pow(n1, n2)
+console.log(`${n1} ^ ${n2} = ${potencia}`)
+
+console.log(Math.max(n1, n2))
+
+// arredonda para um numero mais alto
+console.log(Math.round(n1, n2))
