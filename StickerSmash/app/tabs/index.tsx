e4e9b46a0940cb8c6a03f786509a99255991dbd7
Lokistrange <lucasbrunocc880@outlook.con>
2025-05-15 22:08:15 -0300

    Continuação do StickerSmash

---
+++ StickerSmash/app/tabs/index.tsx
@@ -0,0 +1,38 @@
+import { StyleSheet, View } from 'react-native';
+import Button from '../components/Button';
+import ImageViewer from '../components/ImageViewer';
+const PlaceholderImage = require('@/assets/images/background-image.png');
+
+export default function Index() {
+  return (
+    <View style={styles.container}>
+      <View style={styles.imageContainer}>
+        <ImageViewer imgSource={PlaceholderImage}/>
+      </View>
+      <View style={styles.footerContainer}>
+        <Button theme="primary" label="Escolha uma foto" />
+        <Button label="Usar esta foto" />
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#25292e',
+    alignItems: 'center',
+  },
+  imageContainer: {
+    flex: 1,
+  },
+  image: {
+    width: 320,
+    height: 440,
+    borderRadius: 18,
+  },
+  footerContainer: {
+    flex: 1 / 3,
+    alignItems: 'center'
+  },
+});
